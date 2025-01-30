4fffdd010e05d38ea38d770f54e55954af435706
Tim Schwenke <tim@trallnag.com>
2025-01-30 22:08:33 +0100

    Move to dedicated shell scripts

--- template/scripts/format-with-pre-commit.bash
+++ template/scripts/format-with-pre-commit.bash
@@ -4,5 +4,5 @@ declare -x SKIP
 
 SKIP=$(uv run scripts/filter_pre_commit_hooks.py --mode=tag format)
 
-(pre-commit run --all-files || pre-commit run --all-files) |
-  grep --perl-regexp --invert-match --regexp='\.\e.+Skipped'
+(pre-commit run --all-files || pre-commit run --all-files) \
+  | grep --perl-regexp --invert-match --regexp='\.\e.+Skipped'
